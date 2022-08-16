828c237a9f4f16219630dbeb6f115b52e0721838
Breno Machado <brenomachado0520@gmail.com>
2022-08-16 16:54:14 -0300

    Pastas renomedadas e aprendendo a usar private e public

--- lessons-oo/lesson01/oo-aula01.php
+++ lessons-oo/lesson01/oo-aula01.php
@@ -6,11 +6,13 @@ require('Product.php');
 $prod1 = new Product(null, null, null, null);
 $prod1->setPrice(10);
 $prod1->setName("Macarrão instantaneo");
+//$prod1->name = "Macarrão instantaneo";
 
 echo "Os valores prod1 de são: <br> Nome: ";
 $prod1->getName();
 echo "<br> Preço: ";
 $prod1->getPrice();
+echo "<br> Propriedades: ". $prod1->id . ", " . $prod1->name . " , " . $prod1->image . " , " . $prod1->price;
 echo "<br><br>";
 
 // Setting product 2 using construct method

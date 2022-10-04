1b6a0442b8f46b97d7b3ea34cf788960c72b837e
Breno Machado <brenomachado0520@gmail.com>
2022-10-04 12:05:38 -0300

    Renomeado o campo password para pass

--- database/montapc.sql
+++ database/montapc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Set-2022 às 19:57
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.0.13
+-- Generation Time: Oct 04, 2022 at 05:03 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,42 +18,52 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `montapc`
+-- Database: `montapc`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `password` varchar(512) NOT NULL,
+  `pass` varchar(512) NOT NULL,
   `avatar` varchar(400) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Índices para tabelas despejadas
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `pass`, `avatar`) VALUES
+(1, 'Breno', 'breno@gmail.com', '2d221fa74dbadf392f0bedf7efb96ecc5ec2c8f2', 'https://avatars.githubusercontent.com/u/101892969?v=4'),
+(2, 'Allan', 'allan@gmail.com', '3467d2f86b976ae9f474f73a80f3bfe04b85d8af', 'https://avatars.githubusercontent.com/u/104521672?v=4'),
+(3, 'Micaela', 'micaela@gmail.com', 'e841d81f2858d082ce65cde439a9ae93e4cd1dd0', 'https://avatars.githubusercontent.com/u/110495667?v=4'),
+(4, 'Renan', 'renan@gmail.com', 'c439176a2fa0d1d324255513c10d50a0611a3e34', 'https://avatars.githubusercontent.com/u/4259630?v=4');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
